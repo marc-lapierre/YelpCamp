485d2d73b264fb2f608ff33969b32daf6b78ee58
Marc <12bits@gmail.com>
2022-07-27 20:07:08 -0600

    Added success and error flash messages

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,6 +26,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review)
     await review.save()
     await campground.save()
+    req.flash('success', 'Review submitted 👊')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
@@ -33,6 +34,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
     await Review.findByIdAndDelete(reviewId)
+    req.flash('success', 'Review deleted 👻')
     res.redirect(`/campgrounds/${id}`);
 }))
 
